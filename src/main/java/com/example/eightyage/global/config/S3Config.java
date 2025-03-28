2f01803ec0fdb0b8632e489ec98af641c1549dfe
3uomlkh <3uomlkh@gmail.com>
2025-03-28 18:18:53 +0900

    test(cd) : 배포 테스트용 커밋

--- src/main/java/com/example/eightyage/global/config/S3Config.java
+++ src/main/java/com/example/eightyage/global/config/S3Config.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -14,19 +15,11 @@ public class S3Config {
     @Value("${aws.region}")
     private String region;
 
-    @Value("${aws.access-key}")
-    private String accessKey;
-
-    @Value("${aws.secret-key}")
-    private String secretKey;
-
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)
-                ))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
