a2957ded4c0fd5d8bec7a23e0e32291f31b79f0c
3uomlkh <3uomlkh@gmail.com>
2025-03-29 22:33:58 +0900

    CD 데모 영상을 위한 TEST

--- src/main/java/com/example/eightyage/global/config/S3Config.java
+++ src/main/java/com/example/eightyage/global/config/S3Config.java
@@ -13,10 +13,10 @@ public class S3Config {
 
     private static final String REGION = "ap-northeast-2";
 
-    @Value("${aws.access-key}")
+    @Value("${aws.access.key}")
     private String accessKey;
 
-    @Value("${aws.secret-key}")
+    @Value("${aws.secret.access.key}")
     private String secretKey;
 
 
